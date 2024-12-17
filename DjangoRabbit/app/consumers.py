5a3e06368a3efd8ea3d4fa3c92260f2fa0b86ef6
Daniil <daniilworkmail49@gmail.com>
2024-12-17 06:53:00 +0200

    rabbitmq was added with pika using

---
+++ DjangoRabbit/app/consumers.py
@@ -0,0 +1,19 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='order-queue', durable=True)
+
+def callback(ch, method, properties, body):
+
+    print(f'Received message:{body.decode()}')
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_consume(queue='order-queue', on_message_callback=callback, auto_ack=False)
+
+print('Waiting for the message...')
+
+channel.start_consuming()
---
+++ DjangoRabbit/app/producer.py
@@ -0,0 +1,23 @@
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='order-queue', durable=True)
+
+
+message = 'Hello, RabbitMQ!'
+
+channel.basic_publish(
+    exchange='',
+    routing_key='order-queue',
+    body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=2,
+    )
+)
+
+print(f"Sent message: {message}")
+
+channel.close()
