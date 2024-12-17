0ece8134f1bfc1778504d7028c29da5941be4364
Daniil <daniilworkmail49@gmail.com>
2024-12-17 06:54:53 +0200

    Cycle was removed. Dockstring to view function was added!

--- DjangoRabbit/app/views.py
+++ DjangoRabbit/app/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from app.tasks import add
 
 def test_celery(request):
-    for i in range(100):
-        add.delay(4, 6)
+
+    '''function for celery_rabbitmq testing'''
+
+    add.delay(4, 6)
 
     return HttpResponse("Task is being processed")
